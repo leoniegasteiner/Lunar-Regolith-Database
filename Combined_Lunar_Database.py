0100497d9f9997670fd0a72955d52b3a3043b81a
leonie <gasteinerleonie@gmail.com>
2025-10-21 09:59:30 +0200

    pages

--- Combined_Lunar_Database.py
+++ Combined_Lunar_Database.py
@@ -146,7 +146,7 @@ if db_choice == "Moon Mission Database":
     import os
     import streamlit as st
 
-    MISSION_DIR = "/mount/src/lunar-regolith-database/pages"
+    MISSION_DIR = "pages"
 
     # List available mission scripts
     available_missions = {}
